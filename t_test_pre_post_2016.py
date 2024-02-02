aec06b7eb8309cc4328868c3e6a753bf8ea67abf
bebuma <pa.pongchai@gmail.com>
2024-02-02 17:03:52 +0100

    Reformatted some lines

--- t_test_pre_post_2016.py
+++ t_test_pre_post_2016.py
@@ -1,19 +1,17 @@
 import os
 import pandas as pd
 import statsmodels.api as sm
-import pandas as pd
 import matplotlib.pyplot as plt
 
 # Load the dataset
-absolute_path = os.path.abspath(__file__)
-file_path = os.path.dirname(absolute_path) + "/data_fin_perf.csv"
+file_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), "data_fin_perf.csv")
 data = pd.read_csv(file_path)
 
 # Filter out irrelevant index
 data_filtered = data.drop(columns=['in_index6'])
 
 # Creating a binary variable for pre and post 2016
-data_filtered['pre_post_2016'] = data_filtered['year'].apply(lambda x: 0 if x < 2016 else 1)
+data_filtered['pre_post_2016'] = (data_filtered['year'] >= 2016).astype(int)
 
 # Group data by ticker
 grouped_data = data_filtered.groupby('ticker')
@@ -22,8 +20,6 @@ grouped_data = data_filtered.groupby('ticker')
 ttest_results_sm = {}
 
 for ticker, group in grouped_data:
-    print(ticker)
-    print(group)
     ttest_results_sm[ticker] = {}
     for metric in ['ROE (%)', 'Net Profit Margin (%)', 'Revenue Growth (%)']:
         # Check if the group has enough data for the metric
@@ -43,21 +39,18 @@ for ticker, group in grouped_data:
 # Outputting the results
 print(ttest_results_sm)
 
-# Your result data
-result_data = ttest_results_sm
-
 # Convert the results to a DataFrame for easier processing
 metrics = ['ROE (%)', 'Net Profit Margin (%)', 'Revenue Growth (%)']
 df_results = pd.DataFrame(columns=['Ticker', 'Metric', 'T-Statistic', 'P-Value'])
 
-for ticker, metrics_data in result_data.items():
+for ticker, metrics_data in ttest_results_sm.items():
     for metric, values in metrics_data.items():
         df_results = df_results.append({'Ticker': ticker, 'Metric': metric, 'T-Statistic': values['t_stat'], 'P-Value': values['p_value']}, ignore_index=True)
 
 # Separate DataFrames for each metric
 dfs = {metric: df_results[df_results['Metric'] == metric] for metric in metrics}
 
-# Plotting
+# Plotting and printing results
 for metric, df in dfs.items():
     fig, ax = plt.subplots(figsize=(10, 6))
     ax.bar(df['Ticker'], df['T-Statistic'], label='T-Statistic')
@@ -71,4 +64,3 @@ for metric, df in dfs.items():
     # Print the table of results
     print(f"\nTable of results for {metric}:")
     print(df[['Ticker', 'T-Statistic', 'P-Value']].to_string(index=False))
-
